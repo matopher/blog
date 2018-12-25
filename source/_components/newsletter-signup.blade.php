9e4a2d6ffc729076c9dcfaf8a108b7497351fe47
Matt Woods <mw@mattwoods.io>
2018-12-24 23:56:48 -0600

    updated footer

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -1,38 +1,8 @@
 <div class="flex justify-center lg:-mx-12 my-12 py-12 p-6 md:px-12 bg-grey-lighter border border-grey-light text-sm md:rounded shadow">
-    <!-- Begin Mailchimp Signup Form -->
-    {{-- <div id="mc_embed_signup">
-        <form action="https://mattwoodscreative.us10.list-manage.com/subscribe/post" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-
-            <div id="mc_embed_signup_scroll">
-                <h2 class="text-xl">Like what you see? Sign up for my newsletter.</h2>
-                <p class="text-center text-lg">Every two weeks I'll send you a collection of my most useful & interesting finds. Typically I'll share about marketing, entrepreneurship, software development, productivity and anything else worth sharing. I'll also link to any new articles, videos, or podcast episodes.</p>
-                <div class="mc-field-group">
-                    <label for="MERGE0">Email Address</label>
-                    <input 
-                    type="email" 
-                    value="" 
-                    name="MERGE0" 
-                    class="required email" 
-                    id="MERGE0" 
-                    placeholder="youremail@awesomesauce.com">
-                </div>
-                <input type="hidden" name="u" value="d9c73c89d2fc8dcb8b42ab6d0">
-                <input type="hidden" name="id" value="9478b59355">
-                <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_25582686a9fc051afd5453557_189578c854" tabindex="-1" value=""></div>
-                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-            </div>
-        </form>
-    </div> --}}
-    <!--End Mailchimp Signup Form -->
 
     <div class="flex flex-col items-center text-center max-w-lg">
 
-    <h2 class="text-3xl mb-0">Like what you see? Sign up for my newsletter.</h2>
+    <h2 class="text-3xl mb-0">Get my best, raw insights on software + marketing</h2>
     <p class="text-center text-lg mb-0">Every two weeks I'll send you a collection of my most useful & interesting finds. Typically I'll share about marketing, entrepreneurship, software development, productivity and anything else worth sharing. I'll also link to any new articles, videos, or podcast episodes.</p>
     
     <form
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -80,12 +80,7 @@
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
             <ul class="flex flex-col md:flex-row justify-center list-reset">
                 <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                </li>
-
-                <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+                    &copy; Matt Woods {{ date('Y') }}
                 </li>
             </ul>
         </footer>
