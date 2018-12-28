0ac13f5f750c0f41cae0420a27d31eefdabd71dc
Matt Woods <mw@mattwoods.io>
2018-12-28 01:18:28 -0600

    Added category tags to post preview inline component

--- source/_components/post-preview-inline.blade.php
+++ source/_components/post-preview-inline.blade.php
@@ -1,5 +1,17 @@
 <div class="flex flex-col mb-4">
 
+    <div class="flex-auto w-full container mb-4 mt-2">
+        @if ($post->categories)
+            @foreach ($post->categories as $i => $category)
+                <a
+                    href="{{ '/blog/categories/' . $category }}"
+                    title="View posts in {{ $category }}"
+                    class="inline-block bg-grey-light hover:bg-blue-lighter leading-loose tracking-wide text-grey-darkest uppercase text-xs font-semibold rounded mr-4 px-3 pt-px"
+                >{{ $category }}</a>
+            @endforeach
+        @endif
+    </div>
+
     <h2 class="text-3xl mt-0">
         <a
             href="{{ $post->getUrl() }}"
