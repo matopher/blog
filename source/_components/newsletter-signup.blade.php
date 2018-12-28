842f76f8bda0cacbc149bbab647ed9afcd69f1b0
Matt Woods <mw@mattwoods.io>
2018-12-28 12:30:28 -0600

    update newsletter form copy

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -2,8 +2,8 @@
 
     <div class="flex flex-col items-center text-center max-w-lg">
 
-    <h2 class="text-3xl mb-0">Get my best, raw insights on software + marketing</h2>
-    <p class="text-center text-lg mb-0">Every two weeks I'll send you a collection of my most useful & interesting finds. Typically I'll share about marketing, entrepreneurship, software development, productivity and anything else worth sharing. I'll also link to any new articles, videos, or podcast episodes.</p>
+    <h2 class="text-3xl mb-0">Get my best bi-weekly insights on software + marketing</h2>
+    <p class="text-center text-lg mb-0">Can I send you a collection of my most useful & interesting finds about growth, software & making stuff that matters? I'll also link to any new articles, videos, or podcast episodes.</p>
     
     <form
     class="w-full max-w-md mt-8"
@@ -17,7 +17,7 @@
             text-grey-darkest py-4 px-4 leading-tight
             md:shadow-none shadow-lg mb-6 md:mb-0"
         type="email"
-        placeholder="ambitious-maker@slack.com"
+        placeholder="Your best email"
         aria-label="Email address entry"
         name="MERGE0"
         id="MERGE0"
@@ -42,6 +42,7 @@
       </button>
     </div>
   </form>
+    <small class="pt-2">No spam. Unsubscribe anytime.</small>
 </div>
 
 </div>
