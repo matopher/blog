dc1f6e1a0720a8cc58a915a8c95401a9f9861f67
Matt Woods <mw@mattwoods.io>
2020-01-20 13:53:18 -0600

    Update email signup to use Substack Embed

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -5,44 +5,46 @@
     <h2 class="text-3xl mb-0">Get New Book Summaries in Your Inbox</h2>
     <p class="text-center text-lg mb-0">Every few weeks I'll send you bite-size learnings from battle-tested books to help
         you level-up your life and career. You in?</p>
+
+    <iframe width="100%" height="320" src="https://mattwoods.substack.com/embed" frameborder="0" scrolling="no" class="m-4"></iframe>
     
-    <form
-    class="w-full max-w-md mt-8"
-    action="https://mattwoodscreative.us10.list-manage.com/subscribe/post"
-    method="post"
-  >
-    <div class="block md:flex items-center rounded-lg shadow-none md:shadow-lg bg-tranparent md:bg-white">
-      <input
-        class="bg-white border-none w-full
-            rounded-lg md:rounded-none md:rounded-l-lg
-            text-grey-darkest py-4 px-4 leading-tight
-            md:shadow-none shadow-lg mb-6 md:mb-0"
-        type="email"
-        placeholder="Your best email"
-        aria-label="Email address entry"
-        name="MERGE0"
-        id="MERGE0"
-        autocomplete="email"
-        autocapitalize="off"
-        autocorrect="off"
-      />
+    {{-- <form --}}
+    {{-- class="w-full max-w-md mt-8" --}}
+    {{-- action="https://mattwoodscreative.us10.list-manage.com/subscribe/post" --}}
+    {{-- method="post" --}}
+  {{-- > --}}
+    {{-- <div class="block md:flex items-center rounded-lg shadow-none md:shadow-lg bg-tranparent md:bg-white"> --}}
+    {{--   <input --}}
+    {{--     class="bg-white border-none w-full --}}
+    {{--         rounded-lg md:rounded-none md:rounded-l-lg --}}
+    {{--         text-grey-darkest py-4 px-4 leading-tight --}}
+    {{--         md:shadow-none shadow-lg mb-6 md:mb-0" --}}
+    {{--     type="email" --}}
+    {{--     placeholder="Your best email" --}}
+    {{--     aria-label="Email address entry" --}}
+    {{--     name="MERGE0" --}}
+    {{--     id="MERGE0" --}}
+    {{--     autocomplete="email" --}}
+    {{--     autocapitalize="off" --}}
+    {{--     autocorrect="off" --}}
+    {{--   /> --}}
 
-      <input type="hidden" name="u" value="d9c73c89d2fc8dcb8b42ab6d0">
-      <input type="hidden" name="id" value="9478b59355">
+    {{--   <input type="hidden" name="u" value="d9c73c89d2fc8dcb8b42ab6d0"> --}}
+    {{--   <input type="hidden" name="id" value="9478b59355"> --}}
 
-      <button
-        class="w-full md:w-auto md:flex-no-shrink
-              bg-blue hover:bg-blue-light border-blue
-              hover:border-blue-light uppercase font-bold
-              tracking-wide border-4 text-white py-4 px-4
-              rounded-lg md:rounded-none md:rounded-r-lg
-              text-base md:shadow-none shadow-lg"
-        type="submit"
-      >
-        Subscribe
-      </button>
-    </div>
-  </form>
+    {{--   <button --}}
+    {{--     class="w-full md:w-auto md:flex-no-shrink --}}
+    {{--           bg-blue hover:bg-blue-light border-blue --}}
+    {{--           hover:border-blue-light uppercase font-bold --}}
+    {{--           tracking-wide border-4 text-white py-4 px-4 --}}
+    {{--           rounded-lg md:rounded-none md:rounded-r-lg --}}
+    {{--           text-base md:shadow-none shadow-lg" --}}
+    {{--     type="submit" --}}
+    {{--   > --}}
+    {{--     Subscribe --}}
+    {{--   </button> --}}
+    {{-- </div> --}}
+  {{-- </form> --}}
     <small class="pt-2">No spam. Unsubscribe anytime.</small>
 </div>
 
