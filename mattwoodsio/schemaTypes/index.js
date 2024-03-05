1e00857af59ac01026eefc21b3f76653aa1e073f
Matt Woods <mw@mattwoods.io>
2024-03-04 23:32:18 -0700

    added test pets to schema

--- mattwoodsio/schemaTypes/index.js
+++ mattwoodsio/schemaTypes/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import pet from '../schemas/pet'
+
+export const schemaTypes = [pet]
---
+++ mattwoodsio/schemas/pet.js
@@ -0,0 +1,13 @@
+// schemas/pet.js
+export default {
+    name: 'pet',
+    type: 'document',
+    title: 'Pet',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      }
+    ]
+  }
