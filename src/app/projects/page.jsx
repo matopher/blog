43ddf47d35041ec1bc59e64b963edaed92fc4bfd
Matt Woods <mw@mattwoods.io>
2025-08-08 08:06:14 -0600

    Temporarily hide collison bookshelf project card

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -60,7 +60,7 @@ export default function Projects() {
         role="list"
         className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
       >
-        {projects.map((project) => (
+        {projects.filter(project => project.name !== "Patrick Collison's Bookshelf").map((project) => (
           <Card as="li" key={project.name}>
             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
               <Image
